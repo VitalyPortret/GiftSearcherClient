a9f76752ed3d8ae534c1be30439807002ef11053
portretov-vs-170320 <portretov-vs-170320@fsvps.ru>
2017-10-26 10:15:59 +0300

    Пример из официальной документации Spring Android Добавление пермишина в манифест

--- app/src/main/java/com/giftsearcher/giftsearcherclient/MainActivity.java
+++ app/src/main/java/com/giftsearcher/giftsearcherclient/MainActivity.java
@@ -6,12 +6,30 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvResult;
+    private Button button;
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +46,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JSONTask().execute("http://localhost:8080/api/gifts/expensive");
+            }
+        });
+
+
     }
 
     @Override
@@ -54,8 +83,6 @@ public class MainActivity extends AppCompatActivity {
 
     private class JSONTask extends AsyncTask<String, Void, String> {
 
-        Spinner spinner = new Spinner(MainActivity.this);
-
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -63,12 +90,27 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
-            return JSONHelper.getJsonFromRemoteApi(params[0]);
+            try {
+                final String url = "http://localhost:8080/api/gifts/expensive";
+                RestTemplate restTemplate = new RestTemplate();
+
+// Add the String message converter
+                restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+// Make the HTTP GET request, marshaling the response to a String
+                String result = restTemplate.getForObject(url, String.class, "Android");
+                return result;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+            return null;
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvResult.setText(result);
         }
     }
 }
