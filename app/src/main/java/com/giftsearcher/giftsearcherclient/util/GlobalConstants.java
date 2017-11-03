4783d0381552e0bee4f2e6a099f4bcbcd18db617
ForestGump <forestgump@>
2017-11-03 10:46:17 +0300

    Тестовый вариант дизайна в оранжевом

--- app/src/main/java/com/giftsearcher/giftsearcherclient/util/GlobalConstants.java
+++ app/src/main/java/com/giftsearcher/giftsearcherclient/util/GlobalConstants.java
@@ -2,5 +2,5 @@ package com.giftsearcher.giftsearcherclient.util;
 
 public class GlobalConstants {
 
-    public static final String URL_SERVER = "http://192.168.0.100:8080";
+    public static final String URL_SERVER = "http://10.0.2.2:8080";
 }
